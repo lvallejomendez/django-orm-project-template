a4b1193637c5ec2b93ba0f4bd54536320933b891
lvallejomendez <lvallejomendez@gmail.com>
2023-04-18 07:33:57 -0400

    files for Django template created

---
+++ settings.py
@@ -0,0 +1,15 @@
+# PostgreSQL
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "Mjc3NC1sdmFsbGVq",  # Place with your password generated in Step 1
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+INSTALLED_APPS = ("standalone",)
+
+SECRET_KEY = "SECRET KEY for this Django Project"
